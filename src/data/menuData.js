e0f7a854e07acb2d6a2bfcd62a3238a609adc35c
tandrade <tandrade@atlassian.com>
2019-12-05 16:51:22 +1100

    Setup and some mentor instructions

---
+++ src/data/menuData.js
@@ -0,0 +1,41 @@
+export const menuData = [
+    {
+      emoji: '🍔',
+      label: 'burger',
+      name: 'Cheeseburger',
+      description:
+        'Cheeseburger with pickles, lettuce, tomato and sauce on a sesame seed bun',
+      price: 10
+    },
+    {
+      emoji: '🍲',
+      label: 'ramen',
+      name: 'Vegetarian Ramen',
+      description:
+        'Miso broth with tofu, greens, mushrooms, seaweed and a soft boiled egg',
+      price: 15
+    },
+    {
+      emoji: '🌭',
+      label: 'hotdog',
+      name: 'Hotdog',
+      description: 'Grilled hotdog with sauerkraut, mustard and pickles',
+      price: 8
+    },
+    {
+      emoji: '🥟',
+      label: 'dumpling',
+      name: 'Dumplings',
+      description:
+        'A mix of panfried and steamed pork, prawn and vegetarian dumplngs',
+      price: 10
+    },
+    {
+      emoji: '🥪',
+      label: 'sandwich',
+      name: 'The ultimate toastie',
+      description:
+        'Broccoli, scamorza, parmesan and sriracha mayo toasted on thick sliced bread',
+      price: 8
+    }
+  ];
