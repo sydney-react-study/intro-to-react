a66426eac92e6cc102d7a22a4ec07b99a1103502
tandrade <tandrade@atlassian.com>
2019-12-05 17:28:11 +1100

    Create Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         My first React App
       </div>
     );
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="header__wrapper">
+        <span role="img" aria-label="plates" className="header__emoji">
+          🍽
+        </span>
+        <h1 className="header__title">
+          emoji eats
+          <div className="header__subtitle">fresh seasonal emojis</div>
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
